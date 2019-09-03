d119432168a5e5e52338dea46cd4b2275e9844f8
JohannCox <johann_cox@yahoo.com>
2019-09-03 14:44:53 -0400

    Starting assessment

---
+++ HC_LocalDB_MVVM_WPF/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_LocalDB_MVVM_WPF.Models
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Interests { get; set; }
+        public byte[] ImagesBytes { get; set; }
+
+    }
+}
---
+++ HC_LocalDB_MVVM_WPF/Models/PersonContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HC_LocalDB_MVVM_WPF.Models
+{
+    public class PersonContext: DbContext
+    {
+        public PersonContext() : base("name=PersonContext")
+        {
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+
+    }
+}
